e024a9f62597379dde202159a6eab35031278040
Daniel Gempesaw <gempesaw@gmail.com>
2015-02-08 12:09:32 -0500

    Add travis build badge to primary module

--- default/Module.pm
+++ default/Module.pm
@@ -3,4 +3,13 @@ use warnings;
 package {{$name}};
 
 # ABSTRACT:
+
+=for markdown [![Build Status](https://travis-ci.org/gempesaw/{{$dist->name}}.svg?branch=master)](https://travis-ci.org/gempesaw/{{$dist->name}})
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
 1;
