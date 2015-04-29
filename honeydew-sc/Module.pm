d9937d4e14e0227bfae1e9bcac732b519ec7e894
Daniel Gempesaw <gempesaw@gmail.com>
2015-04-29 12:53:05 -0400

    Create profile for minting dists for the honeydew-sc organization

---
+++ honeydew-sc/Module.pm
@@ -0,0 +1,16 @@
+package {{$name}};
+
+use strict;
+use warnings;
+
+# ABSTRACT:
+
+=for markdown [![Build Status](https://travis-ci.org/honeydew-sc/{{$dist->name}}.svg?branch=master)](https://travis-ci.org/honeydew-sc/{{$dist->name}})
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
