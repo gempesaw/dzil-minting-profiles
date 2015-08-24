e4c7a5a277af0c79b199c6f3a45018bb976086ad
Daniel Gempesaw <gempesaw@gmail.com>
2015-08-24 10:26:36 -0400

    Add Sharecare profile modified from honeydew-sc

---
+++ sharecare/Module.pm
@@ -0,0 +1,15 @@
+package {{$name}};
+
+# ABSTRACT:
+use strict;
+use warnings;
+
+=for markdown [![Build Status](https://travis-ci.org/Sharecare/{{$dist->name}}.svg?branch=master)](https://travis-ci.org/Sharecare/{{$dist->name}})
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
